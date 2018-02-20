cd42bacd698e5f9336b9c100d447b0ce9067a0a6
androidlover5842 <androidlover5842@gmail.com>
2018-02-21 00:01:12 +0530

    include version

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,6 @@ TARGET_KERNEL_CONFIG := lineage_garlic_defconfig
 BOARD_KERNEL_CMDLINE := g_android.luns=1 g_android.removable=1 g_android.cdrom=1 console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 # MR config. MultiROM also uses parts of TWRP config
@@ -61,5 +60,9 @@ MR_DEV_BLOCK_BOOTDEVICE := true
 
 #Force populating from the emmc
 MR_POPULATE_BY_NAME_PATH := "/dev/block/platform/soc/7824900.sdhci/by-name"
+
+include $(LOCAL_PATH)/multirom/version/MR_REC_VERSION.mk
+MR_REC_VERSION := $(shell date -u +%Y%m%d)-01
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board mrom$(MR_REC_VERSION)
 
 include device/generic/twrpbuilder/BoardConfig64.mk
